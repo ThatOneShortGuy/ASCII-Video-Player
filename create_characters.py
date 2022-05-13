bc30d89de154ba2327fb7c0371d53f196b39c17a
Braxton Brown <braxton.brown@outlook.com>
2022-05-13 03:49:56 -0600

    bug fixes

--- create_characters.py
+++ create_characters.py
@@ -1,11 +1,14 @@
 from PIL import Image, ImageFont, ImageDraw
+import os
 
 START = 33
-END = 10000
+END = 383
 
 for i in range(START, END):
     blank = Image.new('RGBA', (14, 18), (0, 0, 0, 0))
     img = ImageDraw.Draw(blank)
     font = ImageFont.truetype('Consolas.ttf', 16)
     img.text((2, 0), chr(i), (255, 255, 255), font)
+    if 'ascii' not in os.listdir():
+        os.mkdir('ascii')
     blank.save(f'ascii\\{i}.png')
