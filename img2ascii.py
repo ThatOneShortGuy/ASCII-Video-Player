17d3b17440fec5c81750e697288795089383fa23
Braxton Brown <braxton.brown@outlook.com>
2023-03-05 15:43:12 -0700

    Improve performace and overall speed

--- img2ascii.py
+++ img2ascii.py
@@ -1,6 +1,5 @@
-from PIL import Image
+import cv2
 import sys
-import numpy as np
 
 
 SIZE = 250, 53
@@ -9,21 +8,20 @@ SIZE = 250, 53
 def load_ascii_map(filename):
     with open(filename, 'rb') as f:
         arr = [chr(int.from_bytes(f.read(2), 'big')) for _ in range(256)]
-    return np.array(arr)
+    return arr
 
 
 def img2ascii(img, ascii_map, size=SIZE):
-    img = img.resize(size)
-    h, w = img.size
-    img = np.array(img)
+    # img = cv2.resize(img, size, cv2.INTER_CUBIC)
+    w, h = img.shape
     return ''.join(''.join(map(lambda x: ascii_map[x], img[i])) + '\n' for i in range(w))
 
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         sys.argv.append('git.png')
-    img = Image.open(sys.argv[1])
-    img = img.convert('L')
+    img = cv2.imread(sys.argv[1])
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     ascii_map = load_ascii_map('ascii_darkmap.dat')
     s = img2ascii(img, ascii_map)
     print(s)
--- video2ascii.py
+++ video2ascii.py
@@ -1,17 +1,18 @@
 from img2ascii import img2ascii, load_ascii_map
 import sys
 import os
-from PIL import Image
+import cv2
 from time import perf_counter
 from threading import Thread
 import shutil
 import vlc
 
+os.add_dll_directory(os.getcwd())
 
 SIZE = 266, 61
 
 
-def read_video(video_path, fps=None):
+def read_video(video_path, fps=None, size=SIZE):
     """
     Reads a video file and returns a list of frames using ffmpeg
     """
@@ -19,22 +20,21 @@ def read_video(video_path, fps=None):
         if not os.path.exists('temp'):
             os.mkdir('temp')
             os.system(f'ffmpeg -y -i "{video_path}" -vn temp/out.flac')
-            os.system(f'ffmpeg -y -i "{video_path}" -q:v 31 {f"-vf fps={fps},scale=800:-1 " if fps else ""}temp/%08d.jpg')
+            os.system(
+                f'ffmpeg -y -i "{video_path}" {f"-vf fps={fps},scale={size[0]}:{size[1]} " if fps else ""}temp/%08d.png')
 
         try:
             img = 1
             p = vlc.MediaPlayer('temp/out.flac')
-            while not os.path.exists(f'temp/{img:08d}.jpg'):
+            while not os.path.exists(f'temp/{img:08d}.png'):
                 pass
             p.play()
             while True:
-                yield Image.open(f'temp/{img:08d}.jpg')
+                yield cv2.imread(f'temp/{img:08d}.png')[:, :, 0]
                 # os.remove(f'temp/{img:08d}.jpg')
                 img += 1
-        except FileNotFoundError:
+        except Exception:
             pass
-        except Exception as e:
-            print(e)
         finally:
             p.release()
     finally:
@@ -42,18 +42,18 @@ def read_video(video_path, fps=None):
         # shutil.rmtree('temp')
 
 
-def show_video(path, fps=55, size=SIZE):
+def show_video(path, fps=60, size=SIZE):
     ifps = 1 / fps
     ascii_map = load_ascii_map('ascii_darkmap.dat')
     start = perf_counter()
-    for frame in read_video(path, fps):
-        frame = frame.convert('L')
+    for frame in read_video(path, fps, size=size):
         s = img2ascii(frame, ascii_map, size)
+        thread = Thread(target=sys.stdout.write, args=(s,))
         while ((end := perf_counter()) - start) < ifps:
             pass
         start = end
         # st = perf_counter()
-        Thread(target=sys.stdout.write, args=(s,)).start()
+        thread.start()
         # print(perf_counter() - st)
         # break
 
@@ -61,7 +61,7 @@ def show_video(path, fps=55, size=SIZE):
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         sys.argv.append('crf18.mp4')
-        sys.argv.append('80')
+        sys.argv.append('85')
         sys.argv.append(SIZE[0])
         sys.argv.append(SIZE[1])
     elif len(sys.argv) < 4:
