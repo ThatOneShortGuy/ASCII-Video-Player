787cb6c159dcadac90a1286a4c40c7851494dc88
Braxton Brown <braxton.brown@outlook.com>
2023-05-25 22:20:35 -0600

    Add build information

---
+++ build.bat
@@ -0,0 +1,42 @@
+@echo off
+
+set COMPRESS_FLAGS="--ultra-brute"
+set COMPRESS=1
+
+if exist dist rmdir /s /q dist
+
+py -3.11 -m nuitka --standalone --include-data-files=ascii_darkmap.dat=ascii_darkmap.dat video2ascii.py
+
+mkdir dist
+
+move video2ascii.dist\_ctypes.pyd dist/
+move video2ascii.dist\ascii_darkmap.dat dist/
+move video2ascii.dist\cimg2ascii.pyd dist/
+move video2ascii.dist\libffi-8.dll dist/
+move video2ascii.dist\python3.dll dist/
+move video2ascii.dist\python311.dll dist/
+move video2ascii.dist\video2ascii.exe dist/
+move video2ascii.dist\cv2 dist\
+move video2ascii.dist\numpy dist\
+
+if %COMPRESS%==0 goto skip_compress
+start "cv2" upx %COMPRESS_FLAGS% dist\cv2\cv2.pyd
+start "cv2 ffmpeg" upx %COMPRESS_FLAGS% dist\cv2\*.dll
+start  "np libopenblas" upx %COMPRESS_FLAGS% dist\numpy\.libs\*
+start "vid2asc" upx %COMPRESS_FLAGS% dist\video2ascii.exe
+start "libffi" upx %COMPRESS_FLAGS% dist\libffi-8.dll
+start "python3" upx %COMPRESS_FLAGS% dist\python3.dll
+start "python311" upx %COMPRESS_FLAGS% dist\python311.dll
+start "ctypes" upx %COMPRESS_FLAGS% dist\_ctypes.pyd
+start "cimg2ascii" upx %COMPRESS_FLAGS% dist\cimg2ascii.pyd
+start "np core" upx %COMPRESS_FLAGS% dist\numpy\core\*
+start "np fft" upx %COMPRESS_FLAGS% dist\numpy\fft\*
+start "np linalg" upx %COMPRESS_FLAGS% dist\numpy\linalg\*
+start "np random" upx %COMPRESS_FLAGS% dist\numpy\random\*
+
+:skip_compress
+rmdir /s /q video2ascii.dist
+rmdir /s /q video2ascii.build
+
+rem Use powershell to create a zip file
+powershell Compress-Archive -Path dist/* -DestinationPath video2ascii.zip -Force -CompressionLevel Optimal
